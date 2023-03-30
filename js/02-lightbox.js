cd67bc6fdb306c68fc7fdf3a5f2c929fdaa23a59
Zyabrik10 <sasamazurok@gmail.com>
2023-03-30 20:39:14 +0300

    Second homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+// init gallery
+// =================================================================
+const li = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`;
+  })
+  .join("\n");
+
+galleryList.insertAdjacentHTML("afterbegin", li);
+
+// prohibit redirections for refernces
+// =================================================================
+document
+  .querySelectorAll(".gallery__link")
+  .forEach((item) =>
+    item.addEventListener("click", (event) => event.preventDefault())
+  );
+
+const gallery = new SimpleLightbox(".gallery li a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
